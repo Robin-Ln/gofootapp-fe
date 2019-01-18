30b5a91a51fab351c367b1777b857c67c4cce524
Robin Louarn <robin.louarn@icloud.com>
2019-01-18 14:00:58 +0100

    Création du service de 'inscription

--- src/app/service/inscription/inscription.service.ts
+++ src/app/service/inscription/inscription.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { ConfigurationService } from '../config/configuration.service';
 import { HttpClient } from 'selenium-webdriver/http';
 import { Configuration } from 'src/app/modele/configuration/configuration';
 
@@ -10,10 +9,7 @@ export class InscriptionService {
 
   config: Configuration;
 
-  constructor(private configService: ConfigurationService, private httpClient: HttpClient) {
-    this.configService.getConfig()
-      .subscribe((data: Configuration) => {
-        this.config = data;
-      });
+  constructor(private httpClient: HttpClient) {
+    this.config = new Configuration();
   }
 }
